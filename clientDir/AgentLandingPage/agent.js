b36cc428b761ad25077fb0c1898f648b28a8ab3d
digitalpatrick <digitalpatrick@outlook.com>
2014-11-20 11:24:24 -0500

    created jason file to fake data

--- clientDir/AgentLandingPage/agent.js
+++ clientDir/AgentLandingPage/agent.js
@@ -1,19 +1,19 @@
 (function () {
     'use strict';
-    
+
     angular.module('app.agent')
     .controller('agentCtrl', agentCtrl);
-    
+
     agentCtrl.$injectt = ['angularLoad', 'dataservice', '$routeParams'];
-    
+
     function agentCtrl (angularLoad, dataservice, $routeParams ){
         var vm = this;
-        
+
         vm.username = ($routeParams.agentuserName);
-        
-        
+
+
         /**load scripts and css here **/
         angularLoad.loadScript('javascripts/scripts.js')
     };
-                    
+
 }());
